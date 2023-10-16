b9f6e4359f50786c0d6dec3e91a8b0aaf8afc508
devingrischow <devingrischow@outlook.com>
2023-10-16 10:19:41 -0500

    uploading work on two connecting screens

--- app/src/main/java/com/example/first_upload/MainActivity.kt
+++ app/src/main/java/com/example/first_upload/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.first_upload
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Intents
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+        //TODO Code
+
+        //set a variable equal to the button OBJECT
+        val nextButton = findViewById<Button>(R.id.nextButton)
+
+        nextButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
